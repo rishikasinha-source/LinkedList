1509a23aaabe0d73a73679e8312b257f898ab04d
rishika7-hub <srishika970@gmail.com>
2020-12-03 14:23:03 +0530

    Welcome to Linked List Program

---
+++ src/com/assignment/LinkedList.java
@@ -0,0 +1,11 @@
+package com.assignment;
+
+public class LinkedList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Linked List Program");
+
+	}
+
+}
